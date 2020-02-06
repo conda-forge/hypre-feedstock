96fe79dda4f643d56709e06596a97b944fd7b90c
Sam Bateman <sam.bateman@nrlssc.navy.mil>
2020-02-06 13:54:21 -0600

    Do not explicitly link mpi library

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,5 @@
-# explicitly link mpi, openblas
-export LDFLAGS="-L$PREFIX/lib -lmpi -llapack -lblas $LDFLAGS"
+# explicitly link openblas
+export LDFLAGS="-L$PREFIX/lib -llapack -lblas $LDFLAGS"
 
 export CC=mpicc
 export CXX=mpicxx
