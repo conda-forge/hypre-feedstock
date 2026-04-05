104abada7fe8ff41a0d01c8cc96eee29f058fabb
Sriman Achanta <68172138+srimanachanta@users.noreply.github.com>
2026-04-05 17:29:26 -0400

    Update recipe to support windows

---
+++ recipe/bld.bat
@@ -0,0 +1,21 @@
+@echo on
+
+mkdir _build
+cd _build
+
+cmake %CMAKE_ARGS% ^
+    -GNinja ^
+    -DHYPRE_SHARED=ON ^
+    -DBUILD_SHARED_LIBS=ON ^
+    -DHYPRE_USING_HYPRE_BLAS=OFF ^
+    -DHYPRE_USING_HYPRE_LAPACK=OFF ^
+    -DHYPRE_USING_FEI=OFF ^
+    -DHYPRE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
+    ../src
+if errorlevel 1 exit 1
+
+ninja -j%CPU_COUNT%
+if errorlevel 1 exit 1
+
+ninja install
+if errorlevel 1 exit 1
